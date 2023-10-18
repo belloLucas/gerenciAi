4cef634bf9505805eafc99f1315dbaab54f1fdbb
belloLucas <lucasbellodev@gmail.com>
2023-10-17 21:14:07 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/gerenciAi/GerenciAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gerenciAi/GerenciAI/GerenciAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciAi.GerenciAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
