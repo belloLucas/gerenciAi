8e14945b2f6d385c3d55b46a652144a8c36f2544
belloLucas <lucasbellodev@gmail.com>
2023-10-20 10:33:05 -0300

    feat: Creating "client" entity

---
+++ src/main/java/br/com/gerenciAi/models/client/Client.java
@@ -0,0 +1,30 @@
+package br.com.gerenciAi.models.client;
+
+import br.com.gerenciAi.models.address.Address;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Client {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String cpf;
+
+    @Embedded
+    private Address address;
+}
